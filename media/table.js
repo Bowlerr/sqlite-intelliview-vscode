4497868ba362b4b02a1585718553e785c14d60a7
Bowlerr <CAWBsprogramming@gmail.com>
2025-12-14 13:55:50 +0000

    feat: expand pagination options to include larger page sizes

--- media/table.js
+++ media/table.js
@@ -7,7 +7,7 @@
 // Pagination settings
 const PAGINATION_CONFIG = {
   defaultPageSize: 100,
-  pageSizeOptions: [50, 100, 200, 500, 1000],
+  pageSizeOptions: [50, 100, 200, 500, 1000, 10000, 100000, 100000],
   maxVisiblePages: 5,
 };
 
